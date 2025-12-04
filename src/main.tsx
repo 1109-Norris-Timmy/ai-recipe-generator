2fba4bdc7709c7411065f382cd7488fbd85a5d34
Norris-Timmy <112657882+1109-Norris-Timmy@users.noreply.github.com>
2025-12-04 11:51:35 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Authenticator } from "@aws-amplify/ui-react";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Authenticator>
-      <App />
+      {({ signOut, user }) => (
+        <App signOut={signOut} user={user} />
+      )}
     </Authenticator>
-</React.StrictMode>
+  </React.StrictMode>
 );
